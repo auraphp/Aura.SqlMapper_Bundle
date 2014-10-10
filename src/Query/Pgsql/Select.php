e7f501fdb8339cf80680a4365366a35c0b8684ea
Paul M. Jones <pmjones88@gmail.com>
2014-10-10 01:07:09 -0500

    first conversion to v2

---
+++ src/Query/Pgsql/Select.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.Sql
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\Sql\Query\Pgsql;
+
+/**
+ *
+ * An object for PgSQL SELECT queries.
+ *
+ * @package Aura.Sql
+ *
+ */
+class Select extends \Aura\Sql\Query\Select
+{
+
+}
