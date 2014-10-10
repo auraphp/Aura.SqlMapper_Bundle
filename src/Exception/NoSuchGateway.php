20a51deba6c3650101b93d4e92954c835db87a3c
Paul M. Jones <pmjones88@gmail.com>
2014-10-10 15:46:38 -0500

    fix namespace

--- src/Exception/NoSuchGateway.php
+++ src/Exception/NoSuchGateway.php
@@ -8,13 +8,13 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  *
  */
-namespace Aura\Sql\Exception;
+namespace Aura\SqlMapper_Bundle\Exception;
 
-use Aura\Sql\Exception;
+use Aura\SqlMapper_Bundle\Exception;
 
 /**
  *
- * No such slave exception
+ * Gateway does not exist.
  *
  * @package Aura.Sql
  *
