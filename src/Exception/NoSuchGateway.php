572ecd1b5ca2fcf9da39b2b56353b02c697c0279
Paul M. Jones <pmjones88@gmail.com>
2014-10-13 16:10:34 -0500

    fix package

--- src/Exception/NoSuchGateway.php
+++ src/Exception/NoSuchGateway.php
@@ -3,7 +3,7 @@
  *
  * This file is part of the Aura Project for PHP.
  *
- * Exception@package Aura.SqlMapper_Bundle
+ * @package Aura.SqlMapper_Bundle
  *
  * @license http://opensource.org/licenses/bsd-license.php BSD
  *
@@ -16,7 +16,7 @@ use Aura\Sql\Exception;
  *
  * No such slave exception
  *
- * Exception@package Aura.SqlMapper_Bundle
+ * @package Aura.SqlMapper_Bundle
  *
  */
 class NoSuchGateway extends Exception
