a9e6ecff1f4e6d3d27c92392c9706772ffee4b1e
Paul M. Jones <pmjones88@gmail.com>
2014-10-20 16:33:21 -0500

    add config placeholder

---
+++ config/Common.php
@@ -0,0 +1,18 @@
+<?php
+namespace Aura\SqlMapper_Bundle\_Config;
+
+use Aura\Di\Config;
+use Aura\Di\Container;
+
+class Common extends Config
+{
+    public function define(Container $di)
+    {
+
+    }
+
+    public function modify(Container $di)
+    {
+
+    }
+}
